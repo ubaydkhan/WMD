94a975e64040d1ff41d25e8248d0fc91d30111bb
ubayd <ubaydkh@gmail.com>
2021-02-16 16:02:31 +0000

    Initial commit

---
+++ src/main/java/uk/ubkh/wmd/WMD.java
@@ -0,0 +1,10 @@
+package uk.ubkh.wmd;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WMD implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/uk/ubkh/wmd/client/WMDClient.java
@@ -0,0 +1,13 @@
+package uk.ubkh.wmd.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class WMDClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
